527b2ef8eab8d36cd5a9b632df400f0c8dd6a1a7
Fuad <fvkfuad@gmail.com>
2021-05-16 13:20:14 +0530

    completed styling of who am i in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,7 +19,7 @@ function Home() {
 
         var element = document.querySelector('.typed-text')
 
-        if(element){
+        if (element) {
             element.textContent = letter;
         }
 
@@ -32,16 +32,27 @@ function Home() {
 
     return (
         <div className="home">
-            <div className="container">
+            <div className="fluid-container">
                 <div class="row align-items-center">
-                    <div class="Intro col-lg-12 col-sm-12 mt-lg-0 mt-4 text-center">
-                        <span class="title-small">Hello</span>
-                        <h1 class="mb-2 title"> <span>I'm</span> Fuad </h1>
-                        <h1 class="mb-4 title">a<span class="typed-text">hi</span></h1>
-                        <div class="mt-sm-5 mt-4">
-                            <a class="btn btn-primary btn-style mr-2" href="contact.html"> Hire Me </a>
+                    <section className="Intro">
+                        <div class="col-lg-12 col-sm-12 mt-lg-0 mt-4 text-center">
+                            <span class="title-small">Hello</span>
+                            <h1 class="mb-2 title"> <span>I'm</span> Fuad </h1>
+                            <h1 class="mb-4 title">a<span class="typed-text">hi</span></h1>
+                            <div class="mt-sm-5 mt-4">
+                                <a class="btn btn-primary btn-style mr-2" href="contact.html"> Hire Me </a>
+                            </div>
                         </div>
-                    </div>
+                    </section>
+
+                    <section className="about">
+                        <div class="col-lg-8 mt-lg-0 mt-5">
+                            <h5 class="title-small mb-2">Who am i?</h5>
+                            <h3 class="title-big">I'm Fuad, a Web Developer</h3>
+                            <p class="mt-4">Hi My Name Is Fuad, I am Studying In 10th Standard.</p>
+                            <a href="about.html" class="btn btn-style btn-primary mt-lg-5 mt-4">Who am i?</a>
+                        </div>
+                    </section>
                 </div>
             </div>
         </div>
